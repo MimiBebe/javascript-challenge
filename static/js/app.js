e57cb8e530c7d225cb98022ac91a25e3b259638f
Tran <tranc27@medtronic.com>
2020-09-25 21:32:30 -0500

    add more filters

--- static/js/app.js
+++ static/js/app.js
@@ -36,22 +36,59 @@ function runFilter() {
     // Select the input element and get the raw HTML node for datetime form
     var dateTimeInputElement = d3.select("#datetime");
 
-    // Get the value property of the input element od datetime filer
+    // Select the input element and get the raw HTML node for city form
+    var cityInputElement = d3.select("#city");
+
+    // Select the input element and get the raw HTML node for state form
+    var stateInputElement = d3.select("#state");
+
+    // Select the input element and get the raw HTML node for country form
+    var countryInputElement = d3.select("#country");
+    
+    // Select the input element and get the raw HTML node for shape form
+    var shapeInputElement = d3.select("#shape");
+
+    // Get the value property of the input element of datetime filer
     var datetimeInput = dateTimeInputElement.property("value");
 
-    // filter data
-    var filteredDatetimeData = tableData.filter(aDataRow => aDataRow.datetime === datetimeInput);
+    // Get the value property of the input element of city filer
+    var cityInput = cityInputElement.property("value").toLowerCase();
 
-    console.log(filteredDatetimeData );
+    // Get the value property of the input element of state filer
+    var stateInput = stateInputElement.property("value").toLowerCase();
+
+    // Get the value property of the input element of country filer
+    var countryInput = countryInputElement.property("value").toLowerCase();
+
+    // Get the value property of the input element of shape filer
+    var shapeInput = shapeInputElement.property("value").toLowerCase();    
+    
+    // Initialize
+    filterTableData = tableData;
+
+    // filter data
+    if (datetimeInput){
+        filterTableData = filterTableData.filter(aDataRow => aDataRow.datetime === datetimeInput);
+    }
+    if (cityInput){
+        filterTableData = filterTableData.filter(aDataRow => aDataRow.city === cityInput);
+    }
+    if (stateInput){
+        filterTableData = filterTableData.filter(aDataRow => aDataRow.state === stateInput);
+    }
+    if (countryInput){
+        ffilterTableData = filterTableData.filter(aDataRow => aDataRow.country === countryInput);
+    }
+    if (shapeInput){
+        filterTableData = filterTableData.filter(aDataRow => aDataRow.shape === shapeInput);
+    }
 
     // clear table body
-    // d3.select("tbody") = "" ;
     d3.selectAll("tbody tr").remove();
-    //d3.select("#ufo-table tbody > tr").empty();
 
     var filterDataBody = d3.select("tbody");
 
-    // display table
-    appendTableData(filteredDatetimeData,filterDataBody);
+    // display new table
+    appendTableData(filterTableData,filterDataBody);
 
   };
